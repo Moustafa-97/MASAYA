1c808ca5bca534122e5359440cc92dbc5d25aded
Moustafa <moustafaadel097@gmail.com>
2024-11-06 21:15:06 +0200

    update

--- src/pages/reserve/reserveHeader/ReserveHeader.tsx
+++ src/pages/reserve/reserveHeader/ReserveHeader.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from "react-i18next";
 import styles from "./reserveHeader.module.css";
 
+
 export default function ReserveHeader() {
   const { t } = useTranslation();
   return (
     <>
       <section className={styles.headerSection}>
         <div className={styles.headerH}>
+          
           <h2>{t("reservation.title")}</h2>
         </div>
         <div className={styles.headerParagraph1}>
