e0ff80a8e0bde8215fab1c6c15e0bd9ea63769f5
Moustafa <moustafaadel097@gmail.com>
2024-11-06 04:15:15 +0200

    update

--- src/pages/NavFooter/NavFooter.tsx
+++ src/pages/NavFooter/NavFooter.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./navFooter.module.css";
 import Footer from "../../components/footer/Footer";
+import { FaWhatsapp } from "react-icons/fa";
 // import { FaWhatsapp } from "react-icons/fa";
 
 export default function NavFooter() {
@@ -18,39 +19,31 @@ export default function NavFooter() {
     <>
       <div
         onClick={handleWhatsAppClick}
+        className={styles.whatsappButton}
         style={{
           cursor: "pointer",
-          width: "fit-content",
-          height: "fit-content",
-          padding:"1rem",
-          minWidth: "3rem",
-          minHeight: "3rem",
-          borderRadius: "5rem",
-          position: "fixed",
-          bottom: "5%",
-          right: "5%",
-          backgroundColor: "var(--beij)",
+          padding: "1rem",
+
           zIndex: "1000",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
         }}
       >
-        <p
+        <div className={styles.whatsappIcon}>
+          <FaWhatsapp />
+        </div>
+        <div
+          className={styles.whatsappText}
           style={{
-            width: "100%",
-            height: "100%",
-            margin: "auto 0",
-            display: "block",
-            color: "var(--red)",
-            fontSize: "clamp(1rem, 4vw, 2rem)",
-            fontWeight: "bold",
-            textAlign: "center",
-            alignSelf:"center",
-            justifySelf:"center",
-
+            // width: "100%",
+            // height: "100%",
+            // margin: "auto 0",
+            // display: "block",
+            // textAlign: "center",
+            // alignSelf: "center",
+            // justifySelf: "center",
           }}
-        >BOOK VIA WHATSAPP</p>
+        >
+          Book via whatsapp
+        </div>
       </div>
       <section className={styles.webpage}>
         <header className={styles.navbarElement}>
